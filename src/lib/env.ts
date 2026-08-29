19f25232ac7581423969bb2501c38e119f0657f1
weifish0 <will060710@gmail.com>
2026-08-30 01:00:47 +0800

    Revert "fix(form): dynamic render home page and add fallback for form url env"
    This reverts commit 8d6c950438de0d545e1e9489f6671fb8bc913039.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,10 +3,5 @@ export function getAppUrl() {
 }
 
 export function getCaseFormUrl() {
-  return (
-    process.env.NEXT_PUBLIC_CASE_FORM_URL ||
-    process.env.GOOGLE_APPS_SCRIPT_WEB_APP_URL ||
-    ""
-  );
+  return process.env.NEXT_PUBLIC_CASE_FORM_URL || "";
 }
-
